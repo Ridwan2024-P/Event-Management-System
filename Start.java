52452dd5dee038bfa90c0572403d6728a88346d9
Ridwan2024-P <ahsanrid@gmail.com>
2024-09-21 10:19:47 +0600

    java

---
+++ Start.java
@@ -0,0 +1,11 @@
+import Classes.*;
+import Interfaces.*;
+import java.lang.*;
+public class Start 
+{
+    public static void main(String[] args) 
+	{
+		Home home=new Home();
+		home.setVisible(true);
+	}
+}
